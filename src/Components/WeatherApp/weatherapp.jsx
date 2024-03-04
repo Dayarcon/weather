83e1813616fcf480c8bb318093fcb48d73ba6e0b
Dayarcon <97634034+Dayarcon@users.noreply.github.com>
2024-03-04 19:14:06 +0530

    creation of alert

--- src/Components/WeatherApp/weatherapp.jsx
+++ src/Components/WeatherApp/weatherapp.jsx
@@ -16,7 +16,7 @@ const Weatherapp = () => {
     const element = document.getElementsByClassName("cityInput")
     if (element[0].value ==="" )
     {
-        return 0;
+        alert("Please Enter City Name")
     }
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
     let response = await fetch(url);
